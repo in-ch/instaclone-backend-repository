f0043ab722136a1f34ae94f86a1fb84f09d14ee2
inch <sxin2949@naver.com>
2021-05-23 19:36:32 +0900

    #4.0 Create Account part one

---
+++ users/users.typeDefs.js
@@ -0,0 +1,26 @@
+import { gql } from "apollo-server";
+
+export default gql`
+    type User{
+        id: Int!
+        firstName: String!
+        lastName: String
+        userName: String!
+        email: String! 
+        createdAt: String! 
+        updatedAt: String! 
+    }
+    type Mutation{
+        createAccount(
+            id: Int!
+            firstName: String!
+            lastName: String
+            userName: String!
+            email: String! 
+            password: String!
+        ):User #User를 반환하는 것이다. 
+    }
+    type Query{
+        seeProfile(username:String!):User 
+    }
+`;
