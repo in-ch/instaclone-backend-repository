5e51958dbb73500c6b2197da2b3e09256149c898
inch <sxin2949@naver.com>
2021-05-30 13:45:33 +0900

    #6.3 Upload Photo part Three

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,11 +1,31 @@
+import client from '../../client';
 import { protectResolver } from '../../users/users.utils';
 
 export default {
     Mutation: {
         uploadPhoto: protectResolver(async(_, {file, caption}, {loggedInUser}) => {
             if(caption){ //parse caption -> get or create Hashtags
-
+                const hashtags = caption.match(/#[\w]+/g);
+                console.log(hashtags);
             }
+            client.photo.create({
+                data: {
+                    file, 
+                    caption,
+                    hashtags: {
+                        connectOrCreate: [
+                            {
+                                where: {
+                                    hashtag: "#"
+                                },
+                                create: {
+                                    hashtag: "#"
+                                }
+                            }
+                        ]
+                    }
+                }
+            })
             // save the photo WITH tahe parsed hashtags 
             // add the photo to the hashtags
         }), 
