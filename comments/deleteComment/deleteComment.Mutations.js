6f36809399962aba7a53ca902bca8eaf710c1c3f
inch <sxin2949@naver.com>
2021-05-31 17:05:38 +0900

    #6.16 Delete Comment and Photos

---
+++ comments/deleteComment/deleteComment.Mutations.js
@@ -0,0 +1,38 @@
+import client from "../../client";
+import { protectResolver } from "../../users/users.utils";
+
+export default {
+    Mutation:{
+        deleteComment:protectResolver(async(_,{id},{loggedInUser})=>{
+            const comment = await client.comment.findUnique({
+                where:{
+                    id
+                },
+                select:{
+                    userId:true
+                }
+            });
+            if(!comment){
+                return {
+                    ok:false,
+                    error: "댓글이 없습니다."
+                }
+            } else if(loggedInUser.id !==comment.userId){
+                return {
+                    ok:false,
+                    error: "Not Authorized"
+                }
+            } else {
+                await client.comment.delete({
+                    where:{
+                        id
+                    }
+                });
+                return {
+                    ok: true
+                }
+            }
+
+        }),
+    }
+}
