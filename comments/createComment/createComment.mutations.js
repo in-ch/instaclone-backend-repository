e147386b961e344585d45c9c2baa5cd2dea81937
inch <sxin2949@naver.com>
2021-05-31 15:48:40 +0900

    Fixed: schema update

--- comments/createComment/createComment.mutations.js
+++ comments/createComment/createComment.mutations.js
@@ -4,6 +4,7 @@ import { protectResolver } from "../../users/users.utils";
 export default {
     Mutation: {
         createComment: protectResolver( async(_,{photoId, payload},{loggedInUser})=> {
+            console.log('Heelo');
             const ok = await client.photo.findUnique({
                 where:{
                     id: photoId
@@ -12,12 +13,14 @@ export default {
                     id:true
                 },
             }); 
+
             if(!ok){
                 return {
                     ok: false,
                     error: '해당 게시글이 없습니다.',
                 }
             }
+
             await client.comment.create({
                 data: {
                     payload,
