63f8e62aac8ca0364031c81c9168019e4725434e
inch <sxin2949@naver.com>
2021-07-15 18:24:42 +0900

    #16.0 Likes part One

--- photos/seePhotoLikes/seePhotoLikes.queries.js
+++ photos/seePhotoLikes/seePhotoLikes.queries.js
@@ -1,17 +1,17 @@
-import client from '../../client';
+import client from "../../client";
 
 export default {
-    Query: {
-        seePhotoLikes: (_,{id}) => {
-            const likes = client.like.findMany({
-                where:{
-                    photoId:id
-                }
-            }).user();
-            return {
-                ok: true,
-                likes : likes
-            }
-        }
-    }
-}
+  Query: {
+    seePhotoLikes: async (_, { id }) => {
+      const likes = await client.like.findMany({
+        where: {
+          photoId: id,
+        },
+        select: {
+          user: true,
+        },
+      });
+      return likes.map((like) => like.user);
+    },
+  },
+};
